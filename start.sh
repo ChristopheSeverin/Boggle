64b743477e269bc5f60f4a8251c0d89c8bd4f99d
Séverin <christophe.cordero@normalesup.org>
2023-02-07 15:08:18 +0100

    Inject javascript inside main.html

--- start.sh
+++ start.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
-
 mkdir -p static
 cd src
 cat var.js input.js boggle.js path.js solutions.js game.js print.js >script.js
-minify script.js >../static/script.js
-rm script.js
+# Inject script.js in main.html 
+awk '
+    match($0, "<script></script>") {
+        print "<script>"
+        while (getline line < jsfile) print line
+        print "</script>"
+        next
+    }
+    {print}
+' jsfile=script.js main.html > main_with_script.html
+minify main_with_script.html >../static/main.html
+rm script.js main_with_script.html
 minify style.css >../static/style.css
-minify main.html >../static/main.html
 minify regles.html >../static/regles.html
 cd ..
 npx esy start
